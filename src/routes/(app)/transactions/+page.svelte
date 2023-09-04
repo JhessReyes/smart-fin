ccb1fa42808631df8106d165f3541da502b104f5
JhessReyes <jhessaureyes@gmail.com>
2023-09-03 19:00:48 -0600

    table

--- src/routes/(app)/transactions/+page.svelte
+++ src/routes/(app)/transactions/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { TRANSACTIONS } from '$lib/api/transactions';
 	import { queryFetch } from '$lib/client';
+	import { Icon } from '$lib/components/atoms';
 	import { Module, Table } from '$lib/components/organisms';
 	import { addToast, appState } from '$lib/stores';
 	import type { TableHead } from '$lib/types';
@@ -8,10 +9,13 @@
 
 	$appState.title = 'Transacciones';
 	let headers: Array<TableHead> = [
-		{ sortable: true, key: 'name', label: 'Nombre' },
+		{ key: 'select', label: '' },
 		{ sortable: true, key: 'description', label: 'Descripcion' },
 		{ sortable: true, key: 'amount', label: 'Monto' },
-		{ sortable: true, key: 'category', label: 'Categoria' }
+		{ sortable: true, key: 'date', label: 'Fecha' },
+		{ sortable: true, key: 'category', label: 'Categoria' },
+		{ key: 'fixed', label: '' },
+		{ key: 'actions', label: '' }
 	];
 
 	let rows: any[] = [];
@@ -31,7 +35,11 @@
 			console.log(response);
 			if (response?.errors || !response?.data) {
 				addToast('Fail!', 'Ocurrió un error al obtener las transacciones', 'error');
-			} else rows = await response?.data?.transactions?.rows;
+			} else
+				rows = await response?.data?.transactions?.rows.map((r) => ({
+					...r,
+					category: r?.category?.name
+				}));
 		}
 	});
 </script>
@@ -45,11 +53,27 @@
 		pageSize={10}
 		showPagination
 	>
-		<div slot="head" let:header>
-			{header.label || ''}
+		<div slot="head" let:header class="w-full flex items-center">
+			{#if header.key === 'select'}
+				<input type="checkbox" class="checkbox checkbox-primary" />
+			{:else}
+				{header.label || ''}
+			{/if}
 		</div>
 		<svelte:fragment slot="body" let:key let:row>
-			{row[key]}
+			{#if key === 'actions'}
+				<Icon name="" className="text-yellow-500" />
+			{:else if key === 'select'}
+				<input type="checkbox" class="checkbox checkbox-primary" />
+			{:else if key === 'fixed'}
+				<Icon name="star" className="text-yellow-500" />
+			{:else if key === 'category'}
+				<div class="badge badge-lg p-6 px-8 w-52 bg-green-100/25 text-green-500 font-bold">
+					{row?.category}
+				</div>
+			{:else}
+				{row[key]}
+			{/if}
 		</svelte:fragment>
 	</Table>
 </Module>
