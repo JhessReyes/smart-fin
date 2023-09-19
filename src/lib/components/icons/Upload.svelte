9d2fcf5dfb7415968cd42f1cc0aa9f2372ca6108
JhessReyes <jhessaureyes@gmail.com>
2023-09-19 04:24:00 -0600

    image upload section

---
+++ src/lib/components/icons/Upload.svelte
@@ -0,0 +1,49 @@
+<script>
+	let className;
+	export { className as class };
+</script>
+
+<svg
+	class={className}
+	xmlns="http://www.w3.org/2000/svg"
+	width="17"
+	height="16"
+	viewBox="0 0 17 16"
+	fill="none"
+>
+	<g clip-path="url(#clip0_699_49)">
+		<path
+			d="M11.1667 10.6667L8.50004 8L5.83337 10.6667"
+			stroke="#518F81"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M8.5 8V14"
+			stroke="#518F81"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M14.0933 12.26C14.7435 11.9055 15.2572 11.3446 15.5532 10.6658C15.8493 9.98691 15.9108 9.22882 15.7281 8.51113C15.5454 7.79343 15.129 7.15701 14.5444 6.7023C13.9599 6.24759 13.2406 6.00049 12.5 6H11.66C11.4582 5.2195 11.0821 4.49489 10.5599 3.88066C10.0378 3.26644 9.3832 2.77857 8.64537 2.45374C7.90754 2.12891 7.10567 1.97557 6.30005 2.00526C5.49443 2.03494 4.70602 2.24687 3.99409 2.62511C3.28216 3.00336 2.66525 3.53807 2.18972 4.18906C1.7142 4.84004 1.39243 5.59036 1.24863 6.3836C1.10482 7.17684 1.14271 7.99235 1.35945 8.76884C1.57619 9.54533 1.96613 10.2626 2.49997 10.8667"
+			stroke="#518F81"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M11.1667 10.6667L8.50004 8L5.83337 10.6667"
+			stroke="#518F81"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</g>
+	<defs>
+		<clipPath id="clip0_699_49">
+			<rect width="16" height="16" fill="white" transform="translate(0.5)" />
+		</clipPath>
+	</defs>
+</svg>
--- src/routes/(app)/transactions/create/+page.svelte
+++ src/routes/(app)/transactions/create/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { Input, Toggle } from '$lib/components/atoms';
+	import { Image, Upload } from '$lib/components/icons';
 	import { appState } from '$lib/stores';
 
 	$appState.title = 'Crear Transacción';
 </script>
 
-<div class="flex justify-between gap-10 h-full w-full">
-	<div role="form" class="card shadow-md w-full m-auto p-12">
+<div class="flex justify-between gap-10 h-full w-full overflow-y-auto">
+	<div role="form" class="card shadow-md w-full h-full m-auto p-12 bg-base-100">
 		<div class="flex flex-col gap-4 w-full">
 			<Input placeholder="Nombre de la Transacción" />
 			<Input placeholder="Monto de la Transacción" props={{ type: 'number' }} />
@@ -19,7 +20,31 @@
 			</div>
 		</div>
 	</div>
-	<section role="complementary" class="card shadow-md w-full m-auto p-12">
-		<div />
+	<section role="complementary" class="card shadow-md w-full h-full m-auto p-12 bg-base-100 overflow-y-auto">
+		<div class="card bg-base-200 p-6 h-72 flex items-center justify-center gap-4">
+			<svelte:component this={Image} class="w-24 h-24" />
+			<div class="flex gap-2 items-center">
+				<svelte:component this={Upload} class="w-4 h-4" />
+				<span class="text-secondary underline"> Subir archivo </span>
+			</div>
+			<div class="flex flex-col items-center text-center">
+				Sube un archivo para tu transacción
+				<span class="text-sm">
+					Formato de archivos <strong> jpeg, png </strong> Tamaño Recomendado
+					<strong> 600x600 (1:1) </strong>
+				</span>
+			</div>
+		</div>
+		<h4>Imagenes Adicionales</h4>
+		<div class="grid grid-cols-2 gap-4 h-auto">
+			<div class="card bg-base-200 p-6 h-auto flex flex-col items-center justify-center gap-4">
+				<svelte:component this={Image} class="w-24 h-24" />
+				<div class="flex gap-2 items-center">
+					<svelte:component this={Upload} class="w-4 h-4" />
+					<span class="text-secondary"> Subir archivo </span>
+				</div>
+			</div>
+			<div class="card p-6 h-72 border-dotted border-4" />
+		</div>
 	</section>
 </div>
